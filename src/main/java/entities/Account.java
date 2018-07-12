17bdaf09ae5b190db68eeee587561829d65ccaed
Hussein Tejan <tejanahmedhu@gmail.com>
2018-07-12 12:42:37 +0100

    created Transaction and Entity

---
+++ src/main/java/entities/Account.java
@@ -0,0 +1,80 @@
+package entities;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Account {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	@Column(length = 32)
+	private String firstName;
+	@Column(length = 32)
+	private String lastName;
+	@Column(length = 4)
+	private String accountNumber;
+	@OneToMany(
+	        mappedBy = "account", 
+	        cascade = CascadeType.ALL, 
+	        orphanRemoval = true
+	    )
+	private List<Transaction> transactions;
+	
+	public Account(long id, String firstName, String lastName, String accountNumber) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountNumber = accountNumber;
+	}
+	
+	public long getID() {
+		return id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+	public String toString() {
+		return "Account ID: "+ getID() + "\n"
+				+ "First name: " + getFirstName() + "\n"
+				+ "Last name: " + getLastName() + "\n"
+				+ "Account number: " + getAccountNumber();
+	}
+	
+
+	
+	
+	
+	public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+ 
+    public void removeTransaction(Transaction transaction) {
+        transactions.remove(transaction);
+    }
+	
+
+}
---
+++ src/main/java/entities/Transaction.java
@@ -0,0 +1,59 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Transaction {
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO)	
+	private long transaction_id;
+	
+	
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ACCOUNT_ID")
+	private Account account;
+	
+	@Column(length = 32)
+	private String name;
+	
+	
+	
+	private Transaction(long transaction_id, String name) {
+		this.transaction_id = transaction_id;
+		this.name = name;
+	}
+	
+	public void setId(long transaction_id) {
+		this.transaction_id = transaction_id;
+	}
+	
+	public long getId() {
+		return transaction_id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
+	public Account getAccount() {
+		return account;
+	}
+
+}
